a94d7004780bb8a33d6cb55a16b0a559bc2231a4
shivakshim <shivakshimankar@gmail.com>
2026-04-11 01:27:09 +0530

    Update WebConfig.java

--- src/main/java/com/project2/urlshortner/rateLimiter/WebConfig.java
+++ src/main/java/com/project2/urlshortner/rateLimiter/WebConfig.java
@@ -17,6 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor)
-                .addPathPatterns("/**"); // VERY IMPORTANT
+        .addPathPatterns("/**")
+        .excludePathPatterns("/error", "/actuator/**");// VERY IMPORTANT
     }
 }
